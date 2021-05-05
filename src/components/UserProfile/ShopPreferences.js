1111b48b90cb5a8f9f82c7b6bebe63835eaba0cc
Ammar Jussa <ammar26497@gmail.com>
2021-05-05 16:40:04 +0500

    Added options table and button

--- src/components/UserProfile/ShopPreferences.js
+++ src/components/UserProfile/ShopPreferences.js
@@ -3,9 +3,15 @@ import UserProfileHeader from '../common/UserProfileHeader';
 
 const ShopPreferences = () => {
   return (
-    <div className="profile-component">
+    <>
       <UserProfileHeader />
-    </div>
+      <div className="d-flex flex-column align-items-center profile-options">
+        <p className="options-title">Shop Perferences</p>
+        <div className="options-table">
+          <button className="options-save-button">Save To Profile</button>
+        </div>
+      </div>
+    </>
   );
 };
 
