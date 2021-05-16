e9a04518b35bae48869820b4b3e614b5e019a7c0
Ammar Jussa <ammar26497@gmail.com>
2021-05-16 17:49:31 +0500

    User Profile complete

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -3,53 +3,110 @@ import UserProfileHead from './UserProfileHead';
 
 //Need separate components for repititive stuff
 
+const Preferences = ({ name }) => {
+  return (
+    <div className="d-flex flex-column align-items-center mb-3">
+      <p>{name}</p>
+      <button className="up-add">Add</button>
+    </div>
+  );
+};
+
 const UserProfile = () => {
   return (
     <div>
       <UserProfileHead />
-      <div className="d-flex align-items-center user-profile">
+      <div className="d-flex user-profile">
         <div className="col-sm-3 d-flex flex-column justify-content-center align-items-center up-preference">
           <p>User Preferences</p>
-          <div className="d-flex flex-column align-items-center mb-3">
-            <p>Diet</p>
-            <button className="up-add">Add</button>
-          </div>
-          <div className="d-flex flex-column align-items-center mb-3">
-            <p>Avoidance</p>
-            <button className="up-add">Add</button>
-          </div>
-          <div className="d-flex flex-column align-items-center mb-3">
-            <p>Dislikes</p>
-            <button className="up-add">Add</button>
-          </div>
-          <div className="d-flex flex-column align-items-center mb-3">
-            <p>Nutrition</p>
-            <button className="up-add">Add</button>
-          </div>
-          <div className="d-flex flex-column align-items-center mb-3">
-            <p>Household</p>
-            <button className="up-add">Add</button>
-          </div>
-          <div className="d-flex flex-column align-items-center mb-3">
-            <p>Preferred Store</p>
-            <button className="up-add">Add</button>
-          </div>
-          <div className="d-flex flex-column align-items-center mb-3">
-            <p>Cooking Experience</p>
-            <button className="up-add">Add</button>
-          </div>
-          <div className="d-flex flex-column align-items-center mb-3">
-            <p>Favorite Cuisine</p>
-            <button className="up-add">Add</button>
-          </div>
+          <Preferences name="Diet" />
+          <Preferences name="Avoidances" />
+          <Preferences name="Dislikes" />
+          <Preferences name="Nutrition" />
+          <Preferences name="Household" />
+          <Preferences name="Preferred Store" />
+          <Preferences name="Cooking experience" />
+          <Preferences name="Favorite Cuisine" />
         </div>
         <div className="col-sm-2"></div>
-        <div className="col-sm-5">
+        <div className="col-sm-5 d-flex flex-column justify-content-center">
           <div className="up-preference">
             <p>Exclude Ingredients</p>
+            <div className="col-sm-4">
+              <div className="d-flex flex-column pb-4">
+                <p>Privacy</p>
+                <div>
+                  <input type="radio" name="privacy" />
+                  <label for="private">Private</label>
+                </div>
+                <div>
+                  <input type="radio" name="privacy" />
+                  <label for="public">Public</label>
+                </div>
+              </div>
+              <div className="d-flex flex-column pb-4">
+                <p>Food Journal</p>
+                <div>
+                  <input type="radio" name="privacy" />
+                  <label for="private">Private</label>
+                </div>
+                <div>
+                  <input type="radio" name="privacy" />
+                  <label for="public">Public</label>
+                </div>
+              </div>
+              <div className="d-flex flex-column">
+                <p>Default Measure</p>
+                <div>
+                  <input type="radio" name="privacy" />
+                  <label for="metric">Metric</label>
+                </div>
+                <div>
+                  <input type="radio" name="privacy" />
+                  <label for="us">US</label>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            <p>User Details</p>
+          <div className="d-flex justify-content-around">
+            <div className="d-flex flex-column align-items-start">
+              <p>User Details</p>
+              <div>
+                <label for="height">Height</label>
+                <input className="up-input" type="text" />
+              </div>
+              <div>
+                <label for="us">Weight (Kg)</label>
+                <input placeholder="100" className="up-input" type="text" />
+              </div>
+              <div>
+                <label for="us">Health Goals</label>
+                <input placeholder="Active" className="up-input" type="text" />
+              </div>
+            </div>
+            <div className="d-flex flex-column align-items-start">
+              <p>Meal Times</p>
+              <div>
+                <label for="us">Breakfast At</label>
+                <input placeholder="07:00" className="up-input" type="text" />
+              </div>
+              <div>
+                <label for="us">Lunch At</label>
+                <input placeholder="13:00" className="up-input" type="text" />
+              </div>
+              <div>
+                <label for="us">Dinner At</label>
+                <input placeholder="19:00" className="up-input" type="text" />
+              </div>
+            </div>
+          </div>
+
+          <div className="pt-5 d-flex justify-content-between align-items-end">
+            <div className="d-flex flex-column">
+              <p>TimeZone</p>
+              <input placeholder="19:00" className="up-input" type="text" />
+            </div>
+            <button className="up-button">Save Profile</button>
           </div>
         </div>
       </div>
