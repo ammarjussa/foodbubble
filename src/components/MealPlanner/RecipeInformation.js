7bb6d9c5925da8b018a06e39c60b28bfa3eded69
Ammar Jussa <ammar26497@gmail.com>
2021-05-17 16:45:42 +0500

    Recipe information done

--- src/components/MealPlanner/RecipeInformation.js
+++ src/components/MealPlanner/RecipeInformation.js
@@ -5,8 +5,8 @@ const Recipe = ({ title, description, img }) => {
   return (
     <div className="d-flex flex-column align-items-center">
       <img className="mp-image" alt="food demo" src={img} />
-      <p>{title}</p>
-      <p>{description}</p>
+      <p className="mp-recipe-title">{title}</p>
+      <p className="mp-recipe-text">{description}</p>
     </div>
   );
 };
@@ -53,7 +53,54 @@ const RecipeInformation = () => {
             img="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
           />
         </div>
-        <p>Customized meal plans or build your own interactive meal planning tool</p>
+        <p className="text text-center">
+          Customized meal plans or build your own interactive meal planning tool
+        </p>
+
+        <div className="mb-5 pb-5">
+          <p className="mp-big text-center mt-5">What's In My Fridge?</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="col-sm-5 d-flex flex-column align-items-center">
+              <p className="text mb-4">
+                Find recipes using what you have in your fridge. <br /> Find recipes that use as
+                many of the ingredients you have available as possible while limiting missing
+                ingredients
+              </p>
+              <button>Try it now</button>
+            </div>
+            <img
+              className="mp-image"
+              alt="food demo"
+              src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+            />
+            <img
+              className="mp-image"
+              alt="food demo"
+              src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+            />
+          </div>
+          <p className="mp-big text-center mt-5">Wine Pairing</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="col-sm-5 d-flex flex-column align-items-center">
+              <p className="text mb-4">
+                Find recipes using what you have in your fridge. <br /> Find recipes that use as
+                many of the ingredients you have available as possible while limiting missing
+                ingredients
+              </p>
+              <button>Try it now</button>
+            </div>
+            <img
+              className="mp-image"
+              alt="food demo"
+              src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+            />
+            <img
+              className="mp-image"
+              alt="food demo"
+              src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+            />
+          </div>
+        </div>
       </div>
     </>
   );
