729c1e3a9f633398d5a88e28a760eb4245ab61fc
Ammar Jussa <ammar26497@gmail.com>
2021-05-23 12:13:19 +0500

    Household page done

--- src/components/common/CounterComponent.js
+++ src/components/common/CounterComponent.js
@@ -2,15 +2,13 @@ import React, { useState } from 'react';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const CounterComponent = () => {
-  const [count, setCount] = useState(0);
-
+const CounterComponent = ({ number, onClickInc, onClickDec }) => {
   return (
     <div className="counter d-flex">
-      <input className="primary-input" value={count} />
+      <input className="primary-input" value={number} />
       <div className="d-flex flex-column justify-content-between align-items-center button-container">
-        <ExpandLessIcon className="counter-icon" onClick={() => setCount(count + 1)} />
-        <ExpandMoreIcon className="counter-icon" onClick={() => setCount(count - 1)} />
+        <ExpandLessIcon className="counter-icon" onClick={onClickInc} />
+        <ExpandMoreIcon className="counter-icon" onClick={onClickDec} />
       </div>
     </div>
   );
