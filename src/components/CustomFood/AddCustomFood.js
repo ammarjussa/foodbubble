6a16db8bdb67e5c44202c19025aec6b7a9332256
Ammar Jussa <ammar26497@gmail.com>
2021-05-29 22:48:14 +0500

    latest commit

--- src/components/CustomFood/AddCustomFood.js
+++ src/components/CustomFood/AddCustomFood.js
@@ -1,60 +1,134 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchHeader from '../common/SearchHeader';
 
+const dietary = [
+  'Vegetarian',
+  'Vegan',
+  'Pescetarian',
+  'Lacto Vegetarian',
+  'Ovo Vegetarian',
+  'Pales',
+  'Primal',
+  'Ketogenic',
+  'Whole30',
+];
+
+const intolerances = [
+  'Diary',
+  'Egg',
+  'Gluten',
+  'Grains',
+  'Peanut',
+  'Seafood',
+  'Sesame',
+  'Shellfish',
+  'Soy',
+  'Tree Nut',
+  'Wheat',
+  'Corn',
+];
+
+const macro = ['Calories', 'Protein (g)', 'Fat(g)', 'Carbs (g)'];
+
+const micro = [
+  [
+    'Alcohol (g)',
+    'Caffine (g)',
+    'Calcium (g)',
+    'Chlorine (g)',
+    'Fiber (g)',
+    'Fluoride (g)',
+    'Copper (g)',
+  ],
+  [
+    'Alcohol (g)',
+    'Caffine (g)',
+    'Calcium (g)',
+    'Chlorine (g)',
+    'Fiber (g)',
+    'Fluoride (g)',
+    'Copper (g)',
+  ],
+  [
+    'Alcohol (g)',
+    'Caffine (g)',
+    'Calcium (g)',
+    'Chlorine (g)',
+    'Fiber (g)',
+    'Fluoride (g)',
+    'Copper (g)',
+  ],
+  [
+    'Alcohol (g)',
+    'Caffine (g)',
+    'Calcium (g)',
+    'Chlorine (g)',
+    'Fiber (g)',
+    'Fluoride (g)',
+    'Copper (g)',
+  ],
+  [
+    'Alcohol (g)',
+    'Caffine (g)',
+    'Calcium (g)',
+    'Chlorine (g)',
+    'Fiber (g)',
+    'Fluoride (g)',
+    'Copper (g)',
+  ],
+];
+
 const MicroNutrition = () => (
-  <div className="d-flex justify-content-between">
-    <div className="d-flex flex-column align-items-center">
-      <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-      <label>Protein (g)</label>
-    </div>
-    <div className="d-flex flex-column align-items-center">
-      <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-      <label>Protein (g)</label>
-    </div>
-    <div className="d-flex flex-column align-items-center">
-      <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-      <label>Protein (g)</label>
-    </div>
-    <div className="d-flex flex-column align-items-center">
-      <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-      <label>Protein (g)</label>
-    </div>
-    <div className="d-flex flex-column align-items-center">
-      <input style={{ width: '5vw' }} className="cr-input" placeholder="0" />
-      <label>Protein (g)</label>
-    </div>
-    <div className="d-flex flex-column align-items-center">
-      <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-      <label>Protein (g)</label>
-    </div>
-    <div className="d-flex flex-column align-items-center">
-      <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-      <label>Protein (g)</label>
-    </div>
+  <div>
+    {micro.map((mic) => (
+      <div className="macro">
+        {mic.map((m) => (
+          <div className="d-flex flex-column align-items-center">
+            <input className="primary-input small-input" placeholder="0" />
+            <label>{m}</label>
+          </div>
+        ))}
+      </div>
+    ))}
   </div>
 );
 
 const AddCustomFood = () => {
+  const [showMicro, setShowMicro] = useState(false);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [box, setBox] = useState('');
+  const [diet, setDiet] = useState([]);
+  const [intol, setIntol] = useState([]);
+
   return (
     <div>
       <SearchHeader title="Add Custom Food" />
       <div className="custom-recipe p-5 mx-5">
-        <div className="d-flex justify-content-between">
-          <div>
+        <div className="row">
+          <div className="col-md-5">
             <div>
-              <p className="global-text1">Name of Food</p>
+              <p className="secondary-title">Name of Food</p>
               <input
                 className="primary-input"
-                style={{ width: '15vw' }}
+                style={{ width: '250px' }}
                 placeholder="Name of the Food"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div>
-              <p className="global-text1">Price Per Serving</p>
-              <input className="primary-input" style={{ width: '10vw' }} placeholder="$" />
+              <p className="secondary-title">Price Per Serving</p>
+              <input
+                className="primary-input"
+                style={{ width: '150px' }}
+                placeholder="$"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
             </div>
             <div className="pb-5 mb-5">
-              <p className="global-text1">Choose Custom Food Box</p>
+              <p className="secondary-title">Choose Custom Food Box</p>
               <select name="food-box" id="food-box">
                 <option value="favorite">Favorite</option>
                 <option value="italian">Italian</option>
@@ -62,167 +136,75 @@ const AddCustomFood = () => {
                 <option value="chinese">Chinese</option>
               </select>
             </div>
-            <div>
-              <p className="global-text1">Macro Nutrition Per Serving</p>
-              <div className="d-flex justify-content-between">
-                <div className="d-flex flex-column align-items-center">
-                  <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-                  <label>Calories</label>
-                </div>
-                <div className="d-flex flex-column align-items-center">
-                  <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-                  <label>Protein (g)</label>
-                </div>
-                <div className="d-flex flex-column align-items-center">
-                  <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-                  <label>Fat (g)</label>
-                </div>
-                <div className="d-flex flex-column align-items-center">
-                  <input style={{ width: '5vw' }} className="primary-input" placeholder="0" />
-                  <label>Carbs (g)</label>
-                </div>
+          </div>
+          <div className="col-md-3">
+            <p className="secondary-title">Dietary</p>
+            {dietary.map((diet) => (
+              <div>
+                <input type="checkbox" id={diet} name="dietary" value={diet} />
+                &nbsp;
+                <label for="veg">{diet}</label>
               </div>
-            </div>
-            <p className="micro-nutrition">Add Micro Nutrition</p>
+            ))}
           </div>
 
+          <div className="col-md-3">
+            <p className="secondary-title marg">Intolerances</p>
+            {intolerances.map((diet) => (
+              <div>
+                <input type="checkbox" id={diet} name="intolerances" value={diet} />
+                &nbsp;
+                <label for="veg">{diet}</label>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="d-flex align-items-center row marg">
+          <div className="col-md-6">
+            <p className="secondary-title">Macro Nutrition Per Serving</p>
+            <div className="macro">
+              {macro.map((mac) => (
+                <div className="d-flex flex-column align-items-center">
+                  <input className="primary-input small-input" placeholder="0" />
+                  <label>{mac}</label>
+                </div>
+              ))}
+            </div>
+            <p
+              className="micro-nutrition"
+              onClick={() => (showMicro ? setShowMicro(false) : setShowMicro(true))}
+            >
+              Add Micro Nutrition
+            </p>
+          </div>
+
+          <div className="col-md-2"></div>
+          <div className="col-md-4 marg">
+            <p className="secondary-title">Upload Image</p>
+            <div className="upload-image">
+              <div className="d-flex flex-column">
+                <input
+                  disabled
+                  style={{ width: '150px' }}
+                  className="cr-input"
+                  placeholder="Choose File"
+                />
+                <label className="text-center">(Max 3MB-JPEG)</label>
+              </div>
+              <button className="ml-5 mb-5 primary-button" style={{ width: '120px' }}>
+                Upload Image
+              </button>
+            </div>
+            <button className="primary-button save">Save Custom Food</button>
+          </div>
+        </div>
+        {showMicro ? (
           <div>
-            <div className="d-flex align-items-center justify-content-center">
-              <div className="pr-5">
-                <p className="global-text1">Dietary</p>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-              </div>
-              <div className="pl-5 ml-5">
-                <p className="global-text1">Intolerances</p>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-                <div>
-                  <input type="checkbox" id="veg" name="veg" value="veg" />
-                  &nbsp;
-                  <label for="veg">Vegetarian</label>
-                </div>
-              </div>
-            </div>
-
-            <div className="p-4">
-              <p className="global-text1">Upload Image</p>
-              <div className="d-flex">
-                <div className="d-flex flex-column">
-                  <input
-                    disabled
-                    style={{ width: '10vw' }}
-                    className="cr-input"
-                    placeholder="Choose File"
-                  />
-                  <label className="text-center">(Max 3MB-JPEG)</label>
-                </div>
-                <button className="p-1 ml-5 mb-5 primary-button">Upload Image</button>
-              </div>
-            </div>
-            <button className="p-1 primary-button" style={{ marginLeft: '57%' }}>
-              Save Custom Food
-            </button>
+            <p className="secondary-title marg">Micro Nutrition</p>
+            <MicroNutrition />
           </div>
-        </div>
-        <div>
-          <p className="global-text1">Micro Nutrition</p>
-          <MicroNutrition />
-          <br />
-          <MicroNutrition />
-          <br />
-          <MicroNutrition />
-          <br />
-          <MicroNutrition />
-          <br />
-          <MicroNutrition />
-          <br />
-          <MicroNutrition />
-        </div>
-        <button className="p-1 mt-5 primary-button" style={{ marginLeft: '88%' }}>
-          Save Custom Food
-        </button>
+        ) : null}
       </div>
     </div>
   );
