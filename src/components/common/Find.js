447d3e5c9d77ab7605ac4c5f3468087db9c2e1fb
Ammar Jussa <ammar26497@gmail.com>
2021-06-03 18:02:06 +0500

    Find responsiveness improved

--- src/components/common/Find.js
+++ src/components/common/Find.js
@@ -5,7 +5,7 @@ const Find = () => {
     <div className="mb-5 pb-5">
       <p className="main-title text-center mt-5">What's In My Fridge?</p>
       <div className="row mt-5">
-        <div className="row">
+        <div className="row find">
           <p className="col-md-6 d-flex flex-column">
             Find recipes using what you have in your fridge. <br /> Find recipes that use as many of
             the ingredients you have available as possible while limiting missing ingredients
@@ -14,12 +14,12 @@ const Find = () => {
             <button className="primary-button align-self-center">Try it Now</button>
           </p>
           <img
-            className="col-md-3"
+            className="col-md-3 mobile"
             alt="food demo"
             src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
           />
           <img
-            className="col-md-3"
+            className="col-md-3 mobile"
             alt="food demo"
             src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
           />
@@ -28,7 +28,7 @@ const Find = () => {
 
       <p className="main-title text-center mt-5">Wine Pairing</p>
       <div className="row mt-5">
-        <div className="row">
+        <div className="row find">
           <p className="col-md-6 d-flex flex-column">
             Find recipes using what you have in your fridge. <br /> Find recipes that use as many of
             the ingredients you have available as possible while limiting missing ingredients
@@ -37,12 +37,12 @@ const Find = () => {
             <button className="primary-button align-self-center">Try it Now</button>
           </p>
           <img
-            className="col-md-3"
+            className="col-md-3 mobile"
             alt="food demo"
             src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
           />
           <img
-            className="col-md-3"
+            className="col-md-3 mobile"
             alt="food demo"
             src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
           />
